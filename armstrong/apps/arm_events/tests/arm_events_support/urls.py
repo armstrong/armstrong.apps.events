d0afc8a7fa563c38003a842a2fe76c6fc26a39bb
Peter Gullekson <pgullekson@baycitizen.org>
2011-04-29 11:49:40 -0700

    removed unused test files

--- armstrong/apps/arm_events/tests/arm_events_support/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('',
-    (r'^profiles/', include('armstrong.core.arm_profiles.urls')),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-)
