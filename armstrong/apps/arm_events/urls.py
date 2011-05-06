9954e2686812d1092802367b5c27b77e6d850605
Peter Gullekson <peter11235@gmail.com>
2011-05-05 17:59:37 -0700

    restricted urls.py querysets to current site

--- armstrong/apps/arm_events/urls.py
+++ armstrong/apps/arm_events/urls.py
@@ -6,11 +6,11 @@ from armstrong.apps.arm_events.views import RSVPCreateView
 
 urlpatterns = patterns('',
 
-    url(r'^$', ListView.as_view(queryset=Event.objects.upcoming()),
+    url(r'^$', ListView.as_view(queryset=Event.on_site.upcoming()),
             name='event_list'),
 
-    url(r'^(?P<slug>[-\w]+)/$', DetailView.as_view(model=Event),
-            name='event_detail'),
+    url(r'^(?P<slug>[-\w]+)/$', DetailView.as_view(model=Event,
+            queryset=Event.on_site.all()), name='event_detail'),
 
     url(r'^(?P<slug>[-\w]+)/rsvp/$', RSVPCreateView.as_view(),
             name='rsvp_create'),
