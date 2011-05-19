07bc0736329355b030724ffd98860d22ec5d33f2
Peter Gullekson <peter11235@gmail.com>
2011-05-19 13:58:56 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,51 @@
-from setuptools import setup
+from distutils.core import setup
+import os
+
+if os.path.exists("MANIFEST"):
+    os.unlink("MANIFEST")
+
+# Borrowed and modified from django-registration
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+
+def build_package(dirpath, dirnames, filenames):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames and 'steps.py' not in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[10:]  # Strip "armstrong<dir separator>"
+        for f in filenames:
+            # Ignore all dot files and any compiled
+            if f.startswith(".") or f.endswith(".pyc"):
+                continue
+            data_files.append(os.path.join(prefix, f))
+
+
+[build_package(dirpath, dirnames, filenames) for dirpath, dirnames, filenames
+        in os.walk('armstrong')]
 
 setup(
     name='armstrong.apps.events',
     version='0.1',
-    description='Provides the events',
+    description='Provides simple events for a Django site.',
     author='Bay Citizen & Texas Tribune',
-    author_email='info@armstrongcms.org',
+    author_email='dev@armstrongcms.org',
     url='http://github.com/armstrongcms/armstrong.apps.events/',
-    packages=[
-        'armstrong',
-        'armstrong.apps',
-        'armstrong.apps.events',
-    ],
-
+    packages=packages,
+    package_data={"armstrong": data_files},
     install_requires=[
-        'distribute',
+        'Django==1.3',
         'armstrong.core.arm_content',
     ],
 
